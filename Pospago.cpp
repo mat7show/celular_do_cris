c6d4f7a69b9bb7dbb66deb28f3f28a072f09db62
João Paulo Santos <joaopaulomdms@gmail.com>
2018-06-30 22:40:28 -0300

    chega dessa loucura

--- Pospago.cpp
+++ Pospago.cpp
@@ -9,10 +9,10 @@ using namespace operadora;
 
 Pospago::Pospago (string numero,const Cliente &dono, list<Chamada> listaChamadas,const DataDMA &vencimento):Celular(numero, dono, listaChamadas)
 {
- interface
+
 vencimento_ = vencimento;
 
-
+}
 
 
 DataDMA Pospago::get_vencimento ()const
@@ -27,4 +27,3 @@ void Pospago::set_vencimento(const DataDMA &vencimento)
 {
 	this->vencimento_ = vencimento;
 }
-
